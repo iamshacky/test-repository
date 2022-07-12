57c3e1c7f3630ef2007ac84a23500c64ab129861
iamshacky <65470293+iamshacky@users.noreply.github.com>
2022-07-12 05:38:05 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 echo "<h1>Hello world</h1>";
 echo "<p>Testing Github";
 echo "<p>Test to see if it updates automatically</p>";
+echo "<p>Test again on 7/12</p>";
 ?>
 <img src="https://assemblykingsllc.com/assets/images/home/assembly-kings-min.jpg" width="800px">
